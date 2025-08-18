5d3035a2c7d0afac1f72c4694803a971cb306b51
Nithya Sudhakar <nithyasudhakar@Nithyas-MacBook-Pro-8.local>
2025-08-18 11:57:36 -0400

    Added things to clean data, made recisions and cleaned it

--- R/clean-data-bad.R
+++ R/clean-data-bad.R
@@ -8,6 +8,27 @@ nlsy <- read_csv("nlsy.csv",
 								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
 								 skip = 1, col_names = nlsy_cols)
 
+#error: dont do setwd, since not everyone has the same wd. So, use HERE package instead, it constructs all the filepaths.
+# TAKEAWAY: USE HERE INSTEAD OF SETWD.
+
+install.packages("here")
+here::here()
+getwd()
+
+setwd("/Users/nithyasudhakar/Desktop/EPI 590 R Git Practice/Data")
+here::here()
+getwd()
+
+#NOTE: sometimes, setwd Data doesnt work and we need the file path to the FOLDER where the data sits.
+
+#Stop
+nlsy <- read_csv("nlsy.csv",
+								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
+								 skip = 1, col_names = nlsy_cols)
+
+
+
+
 library(dplyr)
 nlsy <- nlsy |>
 	mutate(region_cat = factor(region, labels = c("Northeast", "North Central", "South", "West")),
